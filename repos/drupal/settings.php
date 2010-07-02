0f4c7646bc75b8d369e6fcad2bf67137637f8430
David Metzler <metzlerd@32494.no-reply.drupal.org>
2010-07-02 00:53:55 +0000

    Major update with ui refactor. Adding help. #837968 Fixed distributed reports not copying in sub directories.

--- repos/drupal/settings.php
+++ repos/drupal/settings.php
@@ -7,15 +7,25 @@
  * Security provider:  Specify the class name that is used to provide security  
  */
 $conf['access callback'] = 'user_access';
-
+/*
+ * Current user: 
+ * Specificy the php function that will be used to determine the current user.   
+ * Forena provides the following helper functions to for the common drupal defaults
+ *   forena_current_user_uid
+ *   forena_current_user_name
+ */
+$conf['user callback'] = 'forena_current_user_uid'; 
 /*
  * Data provider: 
  * Specify the class name that will be used to interpret data block files.   
  * Note that data blocks in a repository  
- * 
+ * The following commented lines are used as an example. 
  */
+//$conf['data provider'] = 'FrxPDO';
+//$conf['data provider'] = 'FrxPDO';
 
-$conf['data_engine'] = 'FrxDrupal';
+ 
+$conf['data provider'] = 'FrxDrupal';
 
 /*
  * URI: 
@@ -24,4 +34,8 @@ $conf['data_engine'] = 'FrxDrupal';
  * engine it would be the path to the directory containting the files
  */
 // Not applicable to drupal installs
+//$conf['uri'] = 'mysql:host=dbhost;dbname=databasename';
+//$conf['user'] = 'webdbuser'; 
+//$conf['password'] = 'somesecurepassword';
+
 
