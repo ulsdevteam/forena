d4e37d3ef44d7ff19cc679777dc9c6098f29a4ac
David Metzler <metzlerd@evergreen.edu>
2011-07-07 11:47:17 -0700

    Changed user log report to show message instead of location.

--- repos/drupal/user_logs.sql
+++ repos/drupal/user_logs.sql
@@ -1,3 +1,6 @@
 --ACCESS=access administration pages
-SELECT u.uid, u.name, timestamp, location, type, severity, wid from watchdog w JOIN users u on u.uid=w.uid 
+SELECT u.uid, 
+  u.name, timestamp, 
+  message, location, type, severity, wid 
+FROM watchdog w JOIN users u on u.uid=w.uid 
   WHERE u.name=:name ORDER BY timestamp desc
