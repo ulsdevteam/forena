673ccbc6f0ac931cad9112a21ef7c86329537c11
David Metzler <metzler.dl@gmail.com>
2013-07-30 10:35:54 -0700

    Implemented controlled color

--- repos/sample/users_by_state.sql
+++ repos/sample/users_by_state.sql
@@ -4,10 +4,15 @@ select
   name,
   total, 
   CASE 
-    WHEN total>10000 THEN 'good'
+    WHEN total>20000 THEN 'good'
     WHEN total>5000 THEN 'average'
     ELSE 'poor'
-  end AS classification
+  end AS classification,
+  CASE 
+    WHEN total>20000 then 'green'
+    WHEN total>5000 THEN 'yellow'
+    ELSE 'red' 
+  END as  color
 FROM states s join 
   (SELECT state,sum(users) as total from user_distribution
     GROUP BY state) su
