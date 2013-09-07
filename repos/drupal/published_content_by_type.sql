db9c75cfdd0720075e28c55c316736d5a43a0662
David Metzler <metzler.dl@gmail.com>
2013-09-06 18:47:07 -0700

    Updated to use info style format for block specific settings.

--- repos/drupal/published_content_by_type.sql
+++ repos/drupal/published_content_by_type.sql
@@ -1,6 +1,7 @@
 --ACCESS=access content
---ENTITY_TYPE=node
---ENTITY_ID=nid
 SELECT nid, type, title, uid, sticky, promote from node WHERE type=:content_type
   and status=1 
   ORDER BY title
+--INFO
+entity_type = node
+entity_id = nid
