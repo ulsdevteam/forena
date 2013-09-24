b678429a10c6bfa36bf4eb56527b280be0758a61
David Metzler <metzler.dl@gmail.com>
2013-09-23 21:23:43 -0700

    Added the ability to load data from the select statements along with entities. Useful for group by expressions.

--- repos/drupal/published_content_by_type.sql
+++ repos/drupal/published_content_by_type.sql
@@ -1,5 +1,5 @@
 --ACCESS=access content
-SELECT nid, type, title, uid, sticky, promote from node WHERE type=:content_type
+SELECT nid, :content_type as selected_content_type from node WHERE type=:content_type
   and status=1 
   ORDER BY title
 --INFO
