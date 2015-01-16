b98a609d94d165eece7dff611416b0a128e32659
David Metzler <metzlerd@evergreen.edu>
2015-01-15 22:26:41 -0800

    Attempt to upgrade data tables to use columns

--- forena.admin.js
+++ forena.admin.js
@@ -6,10 +6,16 @@
 
   Drupal.behaviors.forenaAdmin = {
     attach: function (context, settings) {
-      $('table.dataTable-paged').dataTable({
-        "sPaginationType": "full_numbers" 
-      }); 
-     
+      if ($.fn.dataTable) {
+        $('table.dataTable-paged').dataTable({
+          "sPaginationType": "full_numbers"
+        });
+
+        $('#forena-block-preview .FrxTable table').dataTable({
+          "sPaginationType": "full_numbers",
+          "scrollX": true
+        });
+      }
     }
   };
 
