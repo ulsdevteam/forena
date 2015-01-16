aa107c506fae96285feaa04fa3eb78f9b7638779
David Metzler <metzlerd@gmail.com>
2015-01-16 07:05:41 -0800

    Fixes for graphing engine and support for skin values.

--- forena.admin.js
+++ forena.admin.js
@@ -7,13 +7,16 @@
   Drupal.behaviors.forenaAdmin = {
     attach: function (context, settings) {
       if ($.fn.dataTable) {
-        $('table.dataTable-paged').dataTable({
+        $('table.dataTable-paged', context).dataTable({
           "sPaginationType": "full_numbers"
         });
 
-        $('#forena-block-preview .FrxTable table').dataTable({
-          "sPaginationType": "full_numbers",
-          "scrollX": true
+        $('#forena-block-preview .FrxTable table', context).dataTable({
+          "sPaginationType" : "full_numbers",
+          "bSort": true,
+          "bAutoWidth": false,
+          "sScrollX": "100%",
+          "bScrollCollapse": true
         });
       }
     }
